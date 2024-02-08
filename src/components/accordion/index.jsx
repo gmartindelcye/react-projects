7d3b576042dd2038f32032d79f18f85c7b17da3e
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-02-08 10:02:01 -0600

    Accordion single selection finished

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -10,16 +10,27 @@ export default function Accordion() {
 
     const [selected, setSelected] = useState(null);
 
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected? null : getCurrentId);
+    }
+
     return (
         <div className="wrapper">
             <div className="accordion">
                 {data && data.length > 0 ? (
                     data.map((dataItem) => (
                         <div className="item">
-                            <div className="title">
+                            <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
                             </div>
+                            {
+                                selected === dataItem.id? (
+                                    <div className="content">
+                                        {dataItem.answer}
+                                    </div>
+                                ) : null
+                            }
                         </div>
                     )
                     )
