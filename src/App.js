150fa3a2ff3e23b9f726fb42f346453b3ef9a265
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-02-08 14:41:28 -0600

    Star Rating finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Accordion from './components/accordion';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
 
       {/* Random Color component */}
       <RandomColor />
+
+      {/* <StarRating /> component */}
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
