ce24acdb034c0d87e505e6674f18b6e3b82d92a0
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-02-08 08:51:35 -0600

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : '1',
+        question : 'What are accordion components?',
+        answer : 'Accordions are a group of collapsible content panels.'
+    },
+    {
+        id : '2',
+        question : 'What are they used for?',
+        answer : 'Accordions are used to show and hide content.'
+    },
+    {
+        id : '3',
+        question : 'Accordion as a musical instrument',
+        answer : 'Accordions are used to play music.'
+    },
+    {
+        id : '4',
+        question : 'Can I create an accordion componente with a different framework?',
+        answer : 'Yes, you can create an accordion component with any framework.'
+    },
+];
+
+export default data;
